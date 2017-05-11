3b29a9583acdded4c56371ee337bf6339db1dd80
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-05-11 19:37:44 -0300

    refactored Raf, being completely static is smarter.

--- src/animation/Raf.ts
+++ src/animation/Raf.ts
@@ -4,14 +4,15 @@ export default class Raf {
 
 	private static _signal = new Signal1<number>();
 	private static _raf = -1;
+
 	private static tick = (time) => {
 		Raf._raf = requestAnimationFrame(Raf.tick);
 		Raf._signal.emit(time);
 	}
 
-	protected _connections:Array<SignalConnection> = [];
+	protected static _connections:Array<SignalConnection> = [];
 
-	connect(fn:any):SignalConnection
+	public static connect(fn:any):SignalConnection
 	{
 		let connection = Raf._signal.connect(fn);
 		this._connections.push(connection);
@@ -19,30 +20,22 @@ export default class Raf {
 		let dispose = connection.dispose;
 		connection.dispose = () => {
 			dispose.call(connection);
-			this.stop();
+			Raf.stop();
 		}
 
-		this.start();
+		Raf.start();
 
 		return connection;
 	}
 
-	public disconnectAll()
-	{
-		while(this._connections.length)
-		{
-			this._connections.pop().dispose();
-		}
-	}
-
-	protected start(){
+	protected static start(){
 		if(Raf._raf == -1 && Raf._signal.hasListeners())
 		{
 			Raf._raf = requestAnimationFrame(Raf.tick);
 		}
 	}
 
-	protected stop(){
+	protected static stop(){
 
 		if(Raf._raf > -1 && !Raf._signal.hasListeners())
 		{
@@ -51,7 +44,7 @@ export default class Raf {
 		}
 	}
 
-	public destruct():void
+	protected static destruct():void
 	{
 		this.disconnectAll();
 
