2dce63dc917b9ccb6aa06045162e269a990eb857
Mient-jan Stelling <mientjan.stelling@gmail.com>
2017-05-11 19:29:38 -0300

    adding promise writeFIle

---
+++ src/promise/node/writeFile.ts
@@ -0,0 +1,17 @@
+let fs = require('fs');
+
+/**
+ *
+ * @param filepath
+ * @param data
+ * @returns {Promise}
+ */
+function writeFile(filepath, data)
+{
+	return new Promise(resolve => {
+
+		fs.readFile(filepath, data, (err) => {
+			resolve();
+		})
+	})
+}
